38add2c1b31ab26a936389eab1708e7b20167ad7
Gopinath Mano <gopimano4242@gmail.com>
2021-08-18 20:53:29 +0530

    First commit

---
+++ js/2.js
@@ -0,0 +1,25 @@
+const regionEurope_spanish = fetch("https://restcountries.eu/rest/v2/lang/es")
+  .then((data) => data.json())
+
+  .then((countries) => countries.forEach((country) => createFlag(country)))
+  .catch((errMsg) => console.log("Data not found"));
+
+function createFlag({ name, flag, population, region, capital }) {
+  const info = document.createElement("div");
+  info.setAttribute("class", "container");
+
+  info.innerHTML = `
+
+    <div class="flag-container">
+     <img class="flag" src=${flag} width="250px" height="150px" alt="Countries-flag">
+   </div>
+<div class="details">
+  <h3>${name} </h3>
+    <p><b>population :</b> ${population} </p>
+    <p><b>Region :</b> ${region} </p>
+    <p><b>Capital :</b> ${capital} </p>
+  </div>
+ 
+    `;
+  document.body.append(info);
+}
---
+++ js/main.js
@@ -0,0 +1,35 @@
+const regionAsia = fetch("https://restcountries.eu/rest/v2/region/asia")
+  .then((data) => data.json())
+  // .then((countries) => countries.forEach((country) => createFlag(country)))
+  .catch((errMsg) => console.log("Data not found"));
+
+const regionEurope = fetch(
+  "https://restcountries.eu/rest/v2/region/europe/languages"
+)
+  .then((data) => data.json())
+  .then((lang) => console.log(lang))
+  // .then((countries) => countries.forEach((country) => createFlag(country)))
+  .catch((errMsg) => console.log("Data not found"));
+
+Promise.all([regionAsia, regionEurope])
+  .then((merge) => merge[0].concat(merge[1]))
+  .then((countries) => countries.forEach((country) => createFlag(country)))
+  .catch((errMsg) => console.log("Data not found"));
+function createFlag({ name, flag, population, region, capital }) {
+  const info = document.createElement("div");
+  info.setAttribute("class", "container");
+
+  info.innerHTML = `
+    <div class="flag-container">
+     <img class="flag" src=${flag} width="250px" height="150px" alt="Countries-flag">
+   </div>
+<div class="details">
+  <h3>${name} </h3>
+    <p><b>population :</b> ${population} </p>
+    <p><b>Region :</b> ${region} </p>
+    <p><b>Capital :</b> ${capital} </p>
+  </div>
+ 
+    `;
+  document.body.append(info);
+}
